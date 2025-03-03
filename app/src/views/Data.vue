c7fe5851def04aa00c45a738cf7b5c86a0003776
Selina227 <selinac33@nycstudents.net>
2025-03-03 12:30:02 -0500

    e

---
+++ app/src/views/Data.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h2>{{ cod.name }}</h2>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
+  const route = useRoute()
+  const cod = ref('')
+  async function getData() {
+    let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json${route.params.id}`)
+    let data = await res.json()
+    cod.value = data
+  }
+  
+  onMounted(() => {
+    getData()
+  })
+  </script>
+  
+  <style scoped></style>
+  
