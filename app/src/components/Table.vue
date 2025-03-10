512adb70bcfd80e1cbfbd046eec2e09b7e78d663
Selina227 <selinac33@nycstudents.net>
2025-03-10 12:30:07 -0400

    e

---
+++ app/src/components/Table.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Cause of Death</h2>
+  </div>
+</template>
+
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<style scoped></style>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Card v-for="(cause, index) in cod" :key="cause.name" :id="index + 1">
+      <h2>{{ cause.leading_cause }}</h2>
+      <h3>{{ cause.deaths }}</h3>
+    </Card>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const cod = ref('')
+async function getData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+  let data = await res.json()
+  cod.value = data.results
+}
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped></style>
